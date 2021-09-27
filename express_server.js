89a15cf47db2557cd21fe474eb65649d655ee3c6
Roya Chobineh <roya.chbn@gmail.com>
2021-09-27 16:53:48 -0400

    commited

--- express_server.js
+++ express_server.js
@@ -4,9 +4,11 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
+  "9sm5xK": {longURL: "http://www.google.com"}
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,4 +29,9 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
 });
